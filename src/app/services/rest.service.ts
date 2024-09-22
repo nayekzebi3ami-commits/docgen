b39d968de9e20f7ffbc317d0af14cf0d0b2f3a2f
Kamel <thomas.giardina.sin@gmail.com>
2024-09-22 23:29:42 +0200

    a

---
+++ src/app/services/rest.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+  private backendUrl: string;
+
+  constructor() {
+    this.backendUrl = environment.production ? 'https://production-backend-url.com' : 'http://localhost:1338';
+  }
+}
